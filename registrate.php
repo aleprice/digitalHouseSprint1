b2e729f2567aeea88f526fc16a74028ed8e9e21e
Raul Villanueva <ravc1004@gmail.com>
2017-05-14 02:35:59 -0300

    Persistencia de Datos dentro del Formulario
    Ya está andando el formulario con persistencia de datos excepto la contraseña. Por otra parte le agrégé a "form" el method y action, le coloqué los values con minúsculas y comentarios para que sea más fácil ver los cambios y la estructura del html.
    Mañana me siento a ver lo de json.

---
+++ registrate.php
@@ -0,0 +1,185 @@
+<?php 
+ //Codigo persistencia de datos 
+$nombre = "";
+$email = "";
+$telefono = "";
+$direccion = "";
+$contraseña = "";
+
+$nombre = (isset($_POST['nombre']) && strlen($_POST['nombre'])) ? $_POST['nombre'] : "Ingrese_su_nombre";
+$email = (isset($_POST['email']) && strlen($_POST['email'])) ? $_POST['email'] : "Ingrese_su_email";
+$telefono = (isset($_POST['telefono']) && strlen($_POST['telefono'])) ? $_POST['telefono'] : "Ingrese_su_telefono";
+$direccion = (isset($_POST['direccion']) && strlen($_POST['direccion'])) ? $_POST['direccion'] : "Ingrese_su_direccion"
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Registrate - Pandora</title>
+  <meta name="Tienda Virtual" content="Pagina de Ropa">
+  <meta name="author" content="Grupo5">
+
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/propio.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!-- HEADER -->
+	<header>
+		<div class="container Logo">
+			<div class="row">
+				<div class="col-sm-12">
+					<img src="img/img_Logo/pandora.jpg"></div>
+				</div>
+			</div>
+		</div>
+		<nav class=" navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="index.html">NombreTiendaWeb</a>
+				</div>
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="index.html">Home</a></li>
+					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Categorías<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Vestidos</a></li>
+							<li><a href="#">Calzados</a></li>
+							<li><a href="#">Accesorio</a></li>
+						</ul>
+					</li>
+					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Mujer<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Vestidos</a></li>
+							<li><a href="#">Calzados</a></li>
+							<li><a href="#">Accesorio</a></li>
+						</ul>
+					</li>
+					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Hombre<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Vestidos</a></li>
+							<li><a href="#">Calzados</a></li>
+							<li><a href="#">Accesorio</a></li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<form class="navbar-form navbar-right">
+							<div class="input-group">
+								<input type="text" class="form-control" placeholder="Search">
+								<div class="input-group-btn">
+									<button class="btn btn-default" type="submit">
+										<i class="glyphicon glyphicon-search"></i>
+									</button>
+								</div>
+							</div>
+						</form>
+					</li>
+					<li><a href="registrate.html"><span class="glyphicon glyphicon-user"></span> Crear Cuenta</a></li>
+					<li><a href="iniciarsesion.html"><span class="glyphicon glyphicon-log-in"></span> Iniciar Sesión </a></li>
+					<li>
+						<div class="checkbox navbar-form fontwhite">
+							<label><input type="checkbox"> Recordame</label>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+
+<!-- FORMULARIO -->
+
+	<div class="container">
+		<div class="row main">
+			<div class="panel-heading">
+				<div class="panel-title text-center">
+					<h2 class="title"> Crea tu cuenta</h2>
+					<hr />
+				</div>
+			</div>			
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+		<!-- agregué method, action y id al tag fom -->
+			<form method="post" action="" id="registrates">
+				<div class="form-group">
+				<!-- pasé mayusculas a minisculas todos los tags de name para que fuese más facil trabajar -->
+					<label for ="Nombre">Nombre:</label>
+					<input type="text" name="nombre" value="<?=$nombre?>" class=form-control id="name" required>	
+				</div>
+				  <div class="form-group">
+    				<label for="email">Email:</label>
+    				<input type="email" name="email" value="<?=$email?>" class="form-control" id="email" required>
+  				</div>
+
+				<div class="form-group">
+					<label for ="telefono">Teléfono (opcional):</label>
+					<input type="tel" name="telefono" value="<?=$telefono?>" class=form-control id="usr">
+				</div>
+				<div class="form-group">
+					<label for ="Dirección">Dirección:</label>
+					<input type="text" name="direccion" value="<?=$direccion?>" class=form-control id="address-line1">
+				</div>
+				<div class="form-group">
+					<label for ="Contraseña">Contraseña:</label>
+					<input type="password" name="contraseña" value="" class=form-control id="psw">
+				</div>
+				<div class="form-group">
+					<label for ="Contraseña">Confirme su contraseña:</label>
+					<input type="password" name="contraseña" value="" class=form-control id="psw-repeat">
+				</div>
+				<div class="form-group">
+					<input type="submit" name="Registrate" value="Registrate" class="btn"> 
+				</div>
+				<div class="form-group">
+					<input type="checkbox"> Recordame
+				</div>
+			</div>
+			</form>
+		</div>
+		
+	</div>
+
+  <footer>
+    <div class="panel-footer backgfooter">
+      <div class="row">
+        <div class="col-sm-4">
+          <ul>
+            <li class="title-li">Nosotros</li>
+            <li><a href="#" class="listfoo">Sobre Pandora</a></li>
+            <li><a href="#" class="listfoo">Política de Privacidad</a></li>
+            <li><a href="faq.html" class="listfoo">Dudas Frecuentes</a></li>
+            <li><a href="#" class="listfoo">Atención al Cliente</a></li>
+            <li><a href="#" class="listfoo">Términos y Condiciones</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-4">
+          <ul>
+            <li class="title-li">Info</li>
+            <li><a href="#" class="listfoo">Como comprar</a></li>
+            <li><a href="#" class="listfoo">Plazos de Entrega</a></li>
+            <li><a href="#" class="listfoo">Promociones Bancarias</a></li>
+            <li><a href="#" class="listfoo">Cambios y devoluciones</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-4">
+          <p>Suscribite a nuestro <button type="button" class="btn btn-success"><a href="#" class="whtfont"> Newsletter</a></button></p>
+          <p>y descubrí nuestras ofertas y premios</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+
+
+</body>
+</html>
